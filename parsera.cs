d5d5be77d16d8bb8de62806a990f05e002ea2893
Alex Shun Yamazaki <alex.yamazaki@outlook.com>
2019-03-30 17:40:25 -0300

    Exercicio parser, letra a)

---
+++ parsera.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compiladores
+{
+    class ExercicioParser
+    {
+        private static int _lookahead;
+        private static List<char> validos = new List<char>();
+
+        public ExercicioParser()
+        {
+            _lookahead = Console.Read();
+            validos.Add('-');validos.Add('+');validos.Add('a');
+        }
+        
+        public void Inicio()
+        {
+            S();
+            Console.WriteLine(_lookahead);
+            if (_lookahead != 13)
+            {
+                Console.WriteLine("Entrada invalida");
+            }
+            else
+            {
+                Console.WriteLine("Entrada valida");
+            }
+        }
+
+
+        public void S()
+        {
+                switch (_lookahead)
+                {
+                    case '+':
+                        match('+');
+                        S();
+                        S();
+                        break;
+                    case '-':
+                        match('-');
+                        S();
+                        S();
+                        break;
+                    case 'a':
+                        match('a');
+                        break;
+                    default:
+                        throw new Exception($"Syntax error! {(char)_lookahead}");
+                }
+        }
+
+
+
+        public void match(int t)
+        {
+            if (t == _lookahead)
+                _lookahead = Console.Read();
+            else
+                throw new Exception("Syntax error");
+        }
+    }
+}
